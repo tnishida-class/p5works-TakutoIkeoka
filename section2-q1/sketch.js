d3e1f3f1a4b93d7948d177caad597c4abe8109e9
TakutoIkeoka <72736588+TakutoIkeoka@users.noreply.github.com>
2020-10-20 12:07:57 +0900

    task2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
+  createCanvas(100,100);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  if(i < 5){
+    noFill()
+    stroke(0, 0, 255);
   }
+  else {
+    stroke(255, 0, 0);
+  }
+    // BLANK[1]
+let x = 10*i + 10
+  ellipse(50,50,x)}
 }
