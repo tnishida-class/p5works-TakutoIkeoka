354d491e50ad6e5ce4f8dfcc7f38e1b97c7ff329
TakutoIkeoka <72736588+TakutoIkeoka@users.noreply.github.com>
2020-10-25 17:38:34 +0900

    taskq2-2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,9 +3,32 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
-  for(let i = 0; i < 8; i++){
+   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      if(i % 2 == 0 && j % 2 == 1){
+        fill(196);
+        rect(size * i, size * j, size, size);
+        if(j == 1){
+          fill(255, 0, 0)
+          ellipse((size*i)+(size/2) ,(size*j)+(size/2) , size)
+        }
+        else if(j == 5 || j == 7){
+          fill(0);
+          ellipse((size*i)+(size/2) ,(size*j)+(size/2) , size)
+        }
+        }
+      else if(i % 2 == 1 && j % 2 == 0){
+        fill(196);
+        rect(size * i, size * j, size, size);
+        if(j == 0 || j == 2){
+          fill(255, 0, 0)
+          ellipse((size*i)+(size/2) ,(size*j)+(size/2) , size)
+        }
+        else if(j == 6){
+          fill(0)
+          ellipse((size*i)+(size/2) ,(size*j)+(size/2) , size)
+        }
+        }
+      }
     }
-  }
 }
