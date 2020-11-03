1e13d03dc8dab6cdf8bc8a444c394503266512c1
TakutoIkeoka <72736588+TakutoIkeoka@users.noreply.github.com>
2020-11-03 14:15:39 +0900

    task3-q1

--- section3-q1/sketch0.js
+++ section3-q1/sketch0.js
@@ -4,18 +4,15 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  fill(0, 51, 160);
-  noStroke();
-  triangle(0, 0, 30, 5, 30, 15);
-  balloon("I love football");
+  balloon("I love keyakizaka46");
 }
 
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 5;
-  fill(0, 51, 160);
-  rect(30, 0, w + p * 2, h + p * 2);
-  fill(255, 255, 0);
-  text(t, p + 30, h);
+  let p = 2;
+  fill(0);
+  rect(0, 0, w + p * 2, h + p * 2);
+  fill(255);
+  text(t, p, h);
 }
