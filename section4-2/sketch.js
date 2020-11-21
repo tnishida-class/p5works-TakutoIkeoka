d044dda94fa7b51f33049d741bd228aa4356f9f5
TakutoIkeoka <72736588+TakutoIkeoka@users.noreply.github.com>
2020-11-21 21:48:20 +0900

    update4-q2

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -27,11 +27,11 @@ function draw(){
   // if(y < 0){ y = height; }
 
 　// 端の処理パターン (2) 跳ね返る
-  if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
-  x = constrain(x, 0, width);
-  y = constrain(y, 0, height);
-}
+//   if(x < 0 || x > width){ vx = -1 * vx; }
+//   if(y > height){ vy = -1 * vy; }
+//   x = constrain(x, 0, width);
+//   y = constrain(y, 0, height);
+// }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
